cc6399a3eead4fe746bd41c568c3759ca446d7af
Sylvain Boschetto <sylvain.boschetto@persee.fr>
2023-08-22 18:51:00 +0200

    add api access individual job listing

--- app.py
+++ app.py
@@ -22,12 +22,17 @@ def show_job(id):
 
   return render_template('jobpage.html', job=job)
 
+@app.route("/api/job/<id>")
+def show_job_json(id):
+  job = load_job_from_db(id)
+  return jsonify(job)
+
 @app.route("/job/<id>/apply", methods=['post'])
 def apply_to_job(id):
   data = request.form
   job = load_job_from_db(id)
   add_application_to_db(id, data)
-  
+
   return render_template('application_submitted.html', application=data, job=job)
 
 if __name__ == "__main__":
